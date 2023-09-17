273a1ff8ef230340055ff3242abe6142f5c76d0b
Mrelshimy <mraafat.elsayed@gmail.com>
2023-09-17 14:09:28 -0700

    Updating file int_pr for betty styling

--- int_pr.c
+++ int_pr.c
@@ -14,7 +14,7 @@ int int_pr(va_list pfargs)
 	int int_ip = va_arg(pfargs, int);
 	int i = 0;
 	int abs_int, last_d, curr_d, tens = 1;
-	
+
 	if (pfargs == NULL)
 		return (-1);
 
@@ -25,9 +25,9 @@ int int_pr(va_list pfargs)
 	if (last_d < 0)
 	{
 		putchar('-');
-		int_ip = - int_ip;
-		last_d = - last_d;
-		abs_int = - abs_int;
+		int_ip = -int_ip;
+		last_d = -last_d;
+		abs_int = -abs_int;
 		i++;
 	}
 
@@ -39,7 +39,7 @@ int int_pr(va_list pfargs)
 			abs_int = abs_int / 10;
 		}
 		abs_int = int_ip;
-		
+
 		while (tens > 0)
 		{
 			curr_d = abs_int / tens;
@@ -50,5 +50,5 @@ int int_pr(va_list pfargs)
 		}
 	}
 	putchar('0' + last_d);
-	return(i);
+	return (i);
 }
