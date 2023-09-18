0eed5935e072c6aed2dbf5e7668168eb1b666055
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-18 14:41:28 -0700

    Uploading u_pr.c o_pr.c x_pr.c X_pr.c to test compile

---
+++ old/u_pr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ *u_pr - encodes and prints unsigned integers
+ *@pfargs : input argument
+ *
+ *Discription Function to encode unsigned integers
+ *
+ *Return: string length
+ */
+
+int u_pr(va_list pfargs)
+{
+	unsigned int value = va_arg(pfargs, unsigned int);
+	int c_ret = 0;
+	int j;
+
+	if (value == 0)
+	{
+		putchar('0');
+		c_ret++;
+	}
+	else
+	{
+
+	int digits[10];
+	int i = 0;
+
+	while (value > 0)
+	{
+		digits[i] = value % 10;
+		value /= 10;
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		putchar(digits[j] + '0');
+		c_ret++;
+	}
+	}
+
+	return (c_ret);
+}
