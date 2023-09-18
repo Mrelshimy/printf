f77a2774f66ef1dccb1736f31c605db0e8cfae3f
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-18 14:44:45 -0700

    Hotfix on main.c file because of error compile %r as unknown

--- old/main.c
+++ old/main.c
@@ -1,29 +1,43 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
-int main()
-  {
-                   int a, b, c, d, e, f;
-                   a = _printf("Hello %b World\n", 9);
-                   printf("a %d\n", a);
-                   b = _printf("Hello %d world\n", 12345);
-                   printf("b %d\n", b);
-                  c = _printf("Hello %srld\n", "wo");
-                  printf("c %d\n", c);
-                  d = _printf("H%cllo world\n", 'e');
-                  printf("d %d\n", d);
-                  e = _printf("%%\n");
-                  printf("e %d\n", e);
-                  f = _printf("%R\n", "ABCDabcd");
-                  _printf("%d\n", f);
-                  _printf("%r\n", "11111 00000 22222");
-                  _printf("==========================Test Task No. 0==========================\n");
-                  _printf("Let's print a simple sentence.\n");
-                 _printf("%s", "I am a string !\n");
-                  _printf("%c\n", 'S');
-                  _printf("%%\n");
-                  _printf("==========================Test Task No. 1==========================\n");
-                  _printf("%d\n", 1024);
-                  _printf("%d\n", -1024);
-                  _printf("%d\n", INT_MAX);
-                  return (0);
- }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    return (0);
+}
