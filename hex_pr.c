9682791545f69e75af9b47ab9c09193098dcdd14
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-18 06:27:26 -0700

    hotfix.v3 hex_pr.c to test compile

---
+++ hex_pr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * hex_pr - function to print #
+ *@pfargs: input argument
+ *
+ *Description: function to print a character input w/o printf
+ *
+ *Return: 1
+ */
+
+int hex_pr(va_list pfargs, const char *format)
+{
+    unsigned int x = va_arg(pfargs, unsigned int);
+    int c_ret = 0;
+
+    if (*format == '#')
+    {
+        putchar('0');
+        putchar('x');
+        c_ret += 2;
+    }
+
+    write(1, &c_ret, 1);
+
+	return (x);
+}
